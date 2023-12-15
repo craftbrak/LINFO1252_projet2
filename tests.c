b56272b7e6444a139bf524ccd57a4c14e9157660
craftbrak <louisdewilde2001@gmail.com>
2023-12-15 10:19:21 +0100

    tests.c

--- tests.c
+++ tests.c
@@ -24,6 +24,26 @@ void debug_dump(const uint8_t *bytes, size_t len) {
     }
 }
 
+void test_exists(int tar_fd) {
+    // Test case: File exists
+    int result = exists(tar_fd, "fichier1");
+    printf("Test file exists: %s\n", result ? "PASS" : "FAIL");
+
+    result = exists(tar_fd, "fichier2");
+    printf("Test file exists: %s\n", result ? "PASS" : "FAIL");
+
+    result = exists(tar_fd, "fichier1");
+    printf("Test file exists: %s\n", result ? "PASS" : "FAIL");
+
+    // Test case: File does not exist
+    result = exists(tar_fd, "nonexistent_file.txt");
+    printf("Test file does not exist: %s\n", result ? "FAIL" : "PASS");
+
+    // Add more tests as needed
+
+    close(tar_fd); // Close the file descriptor
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Usage: %s tar_file\n", argv[0]);
@@ -35,7 +55,7 @@ int main(int argc, char **argv) {
         perror("open(tar_file)");
         return -1;
     }
-
+    test_exists(fd);
     int ret = check_archive(fd);
     printf("check_archive returned %d\n", ret);
 
